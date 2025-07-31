834296bfafecc5e1a57ef27b55aa278cc6799ef0
Dony Yuli <officialdonyyulihandoko@gmail.com>
2025-07-31 22:39:25 +0700

    Commit branch middleware

--- public/index.php
+++ public/index.php
@@ -4,10 +4,11 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use App\App\Router;
 use App\Controller\HomeController;
 use App\Controller\ProductController;
+use App\Middleware\AuthMiddleware;
 
 Router::add("GET", "/", HomeController::class, 'index');
-Router::add('GET', '/hello', HomeController::class, 'hello');
-Router::add('GET', '/world', HomeController::class, 'world');
+Router::add('GET', '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
+Router::add('GET', '/world', HomeController::class, 'world', [AuthMiddleware::class]);
 Router::add("GET", "/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)", ProductController::class, 'categories');
 
 
